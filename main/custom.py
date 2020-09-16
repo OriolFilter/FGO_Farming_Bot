a98bbf241b6b8d473243111e69498b5192481d36
OriolFilter <oriol.filter.7e3@itb.cat>
2020-09-16 13:09:55 +0200

    Now can spinn for GilgaFest

--- main/custom.py
+++ main/custom.py
@@ -51,12 +51,23 @@ if __name__ == '__main__':
         # print(spinnPos)
         prizeResetButtonPos=None
         resetButtonPos=None
+        closeButtonPos=None
         while True:
             client.screenshot()
             # while not client.findPrizeResetButton():
             for x in range (33*5,0,-1):client.click(spinnPos)
-            if not prizeResetButtonPos:prizeResetButtonPos=client.findPrizeResetButton(returnPos=True)
+            if not prizeResetButtonPos:
+                prizeResetButtonPos=client.findPrizeResetButton(returnPos=True)
+                client.screenshot()
             if prizeResetButtonPos:client.click(prizeResetButtonPos)
             sleep(1)
-            if not resetButtonPos:client.findResetButton=client.findResetButton(returnPos=True)
+            if not resetButtonPos:
+                resetButtonPos.findResetButton(returnPos=True)
+                client.screenshot()
             if resetButtonPos:client.click(resetButtonPos)
+            sleep(1)
+            if not closeButtonPos:
+                closeButtonPos=client.findClosePopUpButton(returnPos=True)
+                client.screenshot()
+            if closeButtonPos:client.click(closeButtonPos)
+            sleep(1)
